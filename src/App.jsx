7bbebaf79907316a5db68fade7b4454dcfa32067
Mohamad Rasoul <mohamad.rasoul.work@gmail.com>
2023-11-30 15:34:15 +0400

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
+import Sidebar from "./containers/global/Sidebar";
+import Topbar from "./containers/global/Topbar";
+
+import { ColorModeContext, useMode } from "./config/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
+  const [theme, colorMode] = useMode();
 
   return (
-    <div className='app'>
-      
-    </div>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <AppRoutes />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
