9a0438ba1f289ebb1dcd53991d00baaa0c1fb580
Mohamad Rasoul <mohamad.rasoul.work@gmail.com>
2023-11-30 19:12:38 +0400

    implement header page

--- src/containers/dashboard/index.jsx
+++ src/containers/dashboard/index.jsx
@@ -1,8 +1,34 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../config/theme";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
-    <Box>
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </Box>
       <Typography variant="h1" color="initial">Dashboard</Typography>
     </Box>
   );
